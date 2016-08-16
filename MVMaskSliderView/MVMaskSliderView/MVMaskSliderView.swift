f28fa38c1f35f69c4aaeeff3b4b1ac86b4e64be7
wangcw <fourrings@163.com>
2016-08-16 19:42:51 +0800

    init

---
+++ MVMaskSliderView/MVMaskSliderView/MVMaskSliderView.swift
@@ -0,0 +1,21 @@
+//
+//  MVMaskSliderView.swift
+//  MVMaskSliderView
+//
+//  Created by wangcw on 16/8/15.
+//  Copyright © 2016年 marvin. All rights reserved.
+//
+
+import UIKit
+
+class MVMaskSliderView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
